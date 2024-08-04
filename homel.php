5e9aaad0620c541e9bc2e4ca1c68979933deb8f2
agodwin12 <124671522+agodwin12@users.noreply.github.com>
2024-08-04 19:48:56 +0100

    Add files via upload

---
+++ homel.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet"
+          href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="home.css">
+
+    <title>Home</title>
+</head>
+<body>
+<header>
+    <a href=""class="logo"><Span>G</Span>odwin</a>
+    <ul class="navbar">
+        <li><a href="" class="active">HOME</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">PROJECT</a></li>
+        <li><a href="">BLOG</a></li>
+    </ul>
+    <div class="menu-btn">
+        <span>INTRO</span>
+        <div class="bx bx-menu" id="menu-icon"></div>
+    </div>
+</header>
+
+<section class="home">
+    <div class="home-text">
+        <span>Software Developer</span>
+        <h1>Awah Nde</h1>
+        <p> I build web applications that engage and retain users. Using the best technnologies currently in the world to ensure users security and comfort.</p>
+        <div class="main-btn">
+            <a href="" class="btn">Download CV</a>
+            <a href="" class="btn two">Hire Me</a>
+        </div>
+    </div>
+    
+    <div class="home-img">
+        <img src="images/1.png" alt="">
+    </div>
+</section>
+
+<div class="share">
+    <p>Check Out my:</p>
+    <div class="social">
+        <a href=""><i class="bx bxl-facebook"></i></a>
+        <a href=""><i class="bx bxl-linkedin"></i></a>
+        <a href=""><i class="bx bxl-twitter"></i></a>
+    </div>
+</div>
+
+
+
+<script src="https://unpkg.com/scrollreveal"></script>
+<script src="home.js"></script>
+</body>
+</html>
